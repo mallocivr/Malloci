f9e38c5217a3ddc7100edb05232355a6defd14ec
Michael Gutensohn <michael@gutensohn.me>
2020-04-05 14:36:19 -0700

    updated demo, integrated test page.

--- docs/js/index.js
+++ docs/js/index.js
@@ -12,7 +12,7 @@ document.getElementById('article').innerHTML = marked(md)
 
 let malloci = document.createElement('a-entity')
 
-malloci.setAttribute('malloci', {md: md, API: "http://127.0.0.1:5000/generate"})
-// malloci.setAttribute('malloci', {md: md})
+// malloci.setAttribute('malloci', {md: md, API: "http://127.0.0.1:5000/generate"})
+malloci.setAttribute('malloci', {md: md})
 
 document.querySelector('a-scene').appendChild(malloci)
