4e0f038f8e521a56d691558c6de4d56aa742ad86
Michael Gutensohn <michael@gutensohn.me>
2020-04-18 20:52:33 -0700

    updated.

---
+++ index.js
@@ -0,0 +1,10 @@
+import "aframe"
+import "aframe-extras"
+import hljs from "highlight.js"
+import "aframe-htmlembed-component"
+import "./src/components.js"
+import seedrandom from "seedrandom"
+import VRMD from "./src/vrmd-parser"
+import "./src/css/style.css"
+
+export {VRMD, hljs, seedrandom}
